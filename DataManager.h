a8e71a2807dac9a000276ae9b1e7aba5879993d1
Rafaella Neofytou <rafaella@think-engineer.com>
2019-10-04 16:33:43 +0100

    Changed folders

---
+++ DataManager.h
@@ -0,0 +1,16 @@
+/**
+  * @file    DataManager.cpp
+  * @version 1.0.0
+  * @author  Rafaella Neofytou
+  * @brief   Header file of the DataManager. Maintains the device configuration including sleep and transmit times, as well as raw data formatting.
+  *	     Facilitates storing sensor and application data to EEPROM. 
+  */
+
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes 
+ */
+#include "mbed.h"
+
