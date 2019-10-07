643c4238a08635012e44a513bb908b884a423398
Adam <adam@think-engineer.com>
2019-10-07 13:00:01 +0100

    Basis of data manager class and precompiler parameters

--- DataManager.cpp
+++ DataManager.cpp
@@ -1,12 +1,42 @@
 /**
   * @file    DataManager.cpp
   * @version 1.0.0
-  * @author  Rafaella Neofytou
-  * @brief   C++ file of the DataManager. Maintains the device configuration including sleep and transmit times, as well as raw data formatting.
-  *	     Facilitates storing sensor and application data to EEPROM. 
+  * @author  Rafaella Neofytou, Adam Mitchell
+  * @brief   C++ file of the DataManager. Provides a very lightweight filesystem to facilitate the
+  *          storage of arbritrary file types
   */
 
 /** Includes
  */
 #include "DataManager.h"
+
+
+DataManager::DataManager()
+{
+
+}
+
+
+DataManager::~DataManager()
+{
+
+}
+
+
+uint16_t DataManager::get_max_types()
+{
+	return (uint16_t)TYPE_STORE_LENGTH - sizeof(FileSystem::FileType_t);
+}
+
+
+int DataManager::get_max_records()
+{
+	return (int)RECORD_STORE_LENGTH - sizeof(FileSystem::FileRecord_t);
+}
+
+
+int DataManager::get_storage_size_bytes()
+{
+	return (int)STORAGE_LENGTH;
+}
 
--- DataManager.h
+++ DataManager.h
@@ -1,9 +1,9 @@
 /**
-  * @file    DataManager.cpp
+  * @file    DataManager.h
   * @version 1.0.0
-  * @author  Rafaella Neofytou
-  * @brief   Header file of the DataManager. Maintains the device configuration including sleep and transmit times, as well as raw data formatting.
-  *	     Facilitates storing sensor and application data to EEPROM. 
+  * @author  Rafaella Neofytou, Adam Mitchell
+  * @brief   Header file of the DataManager. Provides a very lightweight filesystem to facilitate the
+  *          storage of arbritrary file types
   */
 
 /** Define to prevent recursive inclusion
@@ -12,5 +12,72 @@
 
 /** Includes 
  */
-#include "mbed.h"
+#include <mbed.h>
 
+/** User must define which persistent storage is being used 
+ */ 
+#define _PERSISTENT_STORAGE_DRIVER STM24256
+
+#if defined (_PERSISTENT_STORAGE_DRIVER) && (_PERSISTENT_STORAGE_DRIVER == STM24256)
+
+#include "STM24256.h"
+
+#define PAGES 500
+#define PAGE_SIZE_BYTES 64
+#define TYPE_STORE_PAGES 2
+#define RECORD_STORE_PAGES 125
+
+#define TYPE_STORE_START_ADDRESS 0
+#define TYPE_STORE_LENGTH PAGE_SIZE_BYTES * TYPE_STORE_PAGES
+#define RECORD_STORE_START_ADDRESS TYPE_STORE_LENGTH
+#define RECORD_STORE_LENGTH PAGE_SIZE_BYTES * RECORD_STORE_PAGES
+#define STORAGE_START_ADDRESS TYPE_STORE_LENGTH + RECORD_STORE_LENGTH
+#define STORAGE_LENGTH (PAGES * PAGE_SIZE_BYTES) - (STORAGE_START_ADDRESS)
+
+namespace FileSystem
+{
+	union FileType_t
+		{
+			struct 
+			{
+				int type_id;
+				int length;
+			} parameters;
+
+			char data[sizeof(FileType_t::parameters)];
+		};
+
+	union FileRecord_t
+	{
+		struct
+		{
+			uint16_t start_address;
+			uint16_t length_bytes;
+			uint16_t record_id;
+			uint8_t type_id;
+			uint8_t UNUSED;
+		} parameters;
+		
+		char data[sizeof(FileRecord_t::parameters)];
+	}
+}
+#endif /* #if defined (_PERSISTENT_STORAGE_DRIVER) && (_PERSISTENT_STORAGE_DRIVER == STM24256) */
+
+
+/** Base class for the Data Manager
+ */ 
+class DataManager
+{
+
+	public:
+
+		DataManager();
+
+		~DataManager();
+
+		uint16_t get_max_types();
+
+		int get_max_records();
+
+		int get_storage_size_bytes();
+}
