4a32f67f6c1ec486e0a81e68b66fa817e84324b8
Adam <adam@think-engineer.com>
2019-11-20 16:38:49 +0000

    Update pre-processor directive formatting

--- DataManager.cpp
+++ DataManager.cpp
@@ -21,9 +21,9 @@ DataManager::DataManager(PinName write_control, PinName sda, PinName scl, int fr
 
 DataManager::~DataManager()
 {
-    #if _PERSISTENT_STORAGE_DRIVER == STM24256
+    #if _PERSISTENT_STORAGE_DRIVER == PS_DRIVER_STM24256xxx
     _storage.~STM24256();
-    #endif /* #if _PERSISTENT_STORAGE_DRIVER == ... */
+    #endif /* #if _PERSISTENT_STORAGE_DRIVER == PS_DRIVER_STM24256xxx */
 }
 
 /** Initialise the file table to all zeros, set file system initialised flag 
